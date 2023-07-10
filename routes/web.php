cdc50f666782744d04812dab8128ada5784075f3
knancia <oliveira.augusto@outlook.com>
2023-07-10 14:16:40 -0300

    Ajuste na rota

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,6 @@ Route::get('/', function () {
     return redirect()->route('home');
 });
 
-
 // Rotas sem autenticação
 Route::get('/home', function () { return Inertia::render('Home'); })->name('home');
 Route::get('/abrirChamado', [AbrirChamado::class, 'index'])->name('abrirChamado');
